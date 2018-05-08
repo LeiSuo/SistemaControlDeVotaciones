4c07589097c51d3a19ca459fc0d723c95421f08d
Joe <>
2018-05-08 17:53:13 -0600

    Classes Added

--- src/java/gov/modelo/Departamento.java
+++ src/java/gov/modelo/Departamento.java
@@ -1,9 +1,36 @@
 package gov.modelo;
 
 /**
- *
+ * Nombre : Usuario
+ * Fecha : May 8, 2018   
+ * Version : 1.0 
  * @author jose_
  */
 public class Departamento {
-    
+    private int idDepartamento;
+    private String nombre;
+
+    public Departamento() {
+    }
+
+    public Departamento(int idDepartamento, String nombre) {
+        this.idDepartamento = idDepartamento;
+        this.nombre = nombre;
+    }
+
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }  
 }
