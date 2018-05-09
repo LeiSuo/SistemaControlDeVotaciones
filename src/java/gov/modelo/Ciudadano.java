31c342b6537d6beb89adc1e66b29f7de21cc3611
Joe <jose_@DESKTOP-M3I8O3Q>
2018-05-08 18:02:07 -0600

    Classes Added

---
+++ src/java/gov/modelo/Ciudadano.java
@@ -0,0 +1,88 @@
+package gov.modelo;
+
+/**
+ * Nombre : Ciudadano
+ * Fecha : May 8, 2018   
+ * Version : 1.0 
+ * @author jose_
+ */
+public class Ciudadano {
+    private String dui;
+    private String nombre;
+    private String apellidos;
+    private Municipio municipio;
+    private String genero;
+    private int edad;
+    private String fechaExpiracion;
+
+    public Ciudadano() {
+    }
+
+    public Ciudadano(String dui, String nombre, String apellidos, Municipio municipio, String genero, int edad, String fechaExpiracion) {
+        this.dui = dui;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.municipio = municipio;
+        this.genero = genero;
+        this.edad = edad;
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Municipio getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(Municipio municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+
+    public void setFechaExpiracion(String fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+    
+    
+}
